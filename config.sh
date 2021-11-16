c68c349f66b35c7f9015be069eafd649b22ab6d1
Bruno Munoz <bruno.munoz@orange.com>
2021-11-16 10:16:22 +0100

    update config

--- config.sh
+++ config.sh
@@ -2,11 +2,29 @@
 # https://github.com/justjanne/powerline-go
 
 function _update_ps1() {
-    PS1="$(powerline-go \
-      -error $? \
+    PS1="$($GOPATH/bin/powerline-go \
+      -error $? -jobs $(jobs -p | wc -l)\
       -path-aliases \~/secwatt_deployment/gits/docker_secwatt/vault_consul=@VAULT)"
+
+    # Uncomment the following line to automatically clear errors after showing
+    # them once. This not only clears the error for powerline-go, but also for
+    # everything else you run in that shell. Don't enable this if you're not
+    # sure this is what you want.
+
+    #set "?"
 }
 
-if [ "$TERM" != "linux" ]; then
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
+
+#
+#function _update_ps1() {
+#    PS1="$(powerline-go \
+#      -error $? \
+#      -path-aliases \~/secwatt_deployment/gits/docker_secwatt/vault_consul=@VAULT)"
+#}
+#
+#if [ "$TERM" != "linux" ]; then
+#    PROMPT_COMMAND="_update_ps1 -shell bash; $PROMPT_COMMAND"
+#fi
